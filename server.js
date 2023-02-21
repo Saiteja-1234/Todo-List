55c998ec098ee373ae751edd72853dadd293162a
Saiteja-1234 <saiteja.miriyala01@gmail.com>
2023-02-21 15:53:14 +0530

    abc

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const express = require('express');
 require("dotenv").config();
 const app = express();
 
-const port = process.env.PORT || 5030;
+// const port = process.env.PORT || 3000;
+const port=3000;
 
 app.use(express.json());
 app.use(express.static("frontend"));
