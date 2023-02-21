50f06489d39f79f131a3121555be1145760b62b3
Saiteja-1234 <saiteja.miriyala01@gmail.com>
2023-02-21 15:50:03 +0530

    abc

--- server.js
+++ server.js
@@ -1,15 +1,38 @@
+const todoLib = require("./backend/Lib/todoLib");
+const mongoose=require("mongoose");
 const express = require('express');
+require("dotenv").config();
 const app = express();
 
-const port = process.env.PORT || 9030;
+const port = process.env.PORT || 5030;
 
+app.use(express.json());
 app.use(express.static("frontend"));
 
 
 app.get("/api/todos", function(req, res) {
-    res.json([
-        { name: "todo1", iscompleted: true }, { name: "todo1", iscompleted: true }, { name: "todo1", iscompleted: true }
-    ]);
+    todoLib.getAllTodos((err,todos)=>{
+        if(err){
+            res.json({status : "error",message : err, data : null});
+        }
+        else{
+            res.json({status:"Success", data : todos}); 
+        }
+    });
+});
+
+// create todo app api's
+app.post("/api/todos", function(req, res){
+	const todo = req.body;
+    console.log(todo);
+	todoLib.createTodo(todo, function(err, dbtodo){
+		if(err){
+			res.json({status: "error", message: err, data: null});
+		}
+		else{
+			res.json({status: "success", data: dbtodo});
+		}
+	})
 });
 
 app.use("/", function(req, res) {
@@ -17,7 +40,25 @@ app.use("/", function(req, res) {
 });
 
 
+mongoose.set('strictQuery', true);
+mongoose.connect(
+    process.env.MONGO_CONNECTION_STRING, {}, (err) => {
+        if (err) {
+            console.error(err);
+            return;
+        } // else {
+        console.log("Database Connected");
 
-app.listen(port, function() {
-    console.log("Sever running on http://localhost:" + port);
-});
+        // do not create user if already exist
+
+        // userLib.getSingleUser({ username: "Sandeep1729" })
+        // connecting server with port
+        app.listen(port, () => {
+            console.log(`Server Running on http://localhost:${port}`);
+        });
+        // }
+    }
+);
+// app.listen(port, function() {
+//     console.log("Sever running on http://localhost:" + port);
+// });
